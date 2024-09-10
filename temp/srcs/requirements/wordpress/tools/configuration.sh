190cf80b31f02b20a78516b7bcff6b3349b7f168
Changhyun Lee <changhyu@c2r2s3.42gyeongsan.kr>
2024-09-10 13:00:41 +0900

    add

--- temp/srcs/requirements/wordpress/tools/configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-    mkdir -p /var/www/html
-    cd /var/www/html
-    wp core download --allow-root
-    wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
-    wp core install --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-    wp user create ${WP_USER} $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
-
-/usr/sbin/php-fpm81 -F
