894590abdee6a9b15be977f565b3fa1b7fe75749
Changhyun Lee <changhyu@c2r2s3.42gyeongsan.kr>
2024-09-10 11:55:56 +0900

    add

---
+++ temp/tools/configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+    mkdir /etc/nginx/ssl
+    openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
+            -subj "/C=KR/ST=Gyeongsangbuk-do/L=Gyeongsan/O=42Gyeongsan/OU=cadet/CN=taejikim" \
+            -keyout /etc/nginx/ssl/taejikim.key -out /etc/nginx/ssl/taejikim.crt
+    mkdir -p /run/nginx
+
+# nginx -g "daemon off;"
+exec /bin/sh
