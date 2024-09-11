b5088a0cbec4ab7b9bed7979fce586116003e55f
Changhyun Lee <changhyu@c2r7s1.42gyeongsan.kr>
2024-09-11 12:37:51 +0900

    add

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -8,7 +8,7 @@ mysqld &
         sleep 1
     done
 
-mysql -uroot <<EOF
+mysql -uroot --skip-password <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
